bb0d2d86ea4d1ae280c30d371ee763d76edcb5c8
Fredrik Henriksen Lövlie <fredriklovlie@gmail.com>
2020-09-23 09:22:24 +0200

    Project push

---
+++ Assets/Scripts/RadialTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class RadialTrigger : MonoBehaviour
+{
+    [SerializeField] private float CircleRadius = 5.0f;
+    private void Update()
+    {
+        Handles.DrawWireDisc(transform.position, Vector3.forward, CircleRadius);
+    }
+}
